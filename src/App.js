3e92ce76400f5c8ac05e639a808f50a13aaf587c
zTjamm <jamm85@bk.ru>
2022-06-21 08:48:00 +0300

    all done
    step del

--- src/App.js
+++ src/App.js
@@ -8,28 +8,38 @@ import Filter from "./components/Filter/Filter";
 export default class App extends Component {
     state = {
         contacts: [
-            {id:1, name: "Mikl", number: "555-55-55"},
-            {id:2, name: "Alesha", number: "777-77-77"},
-            {id:3, name: "Elena", number: "999-99-99"}
+            {id: 1, name: "Mikl", number: "555-55-55"},
+            {id: 2, name: "Alesha", number: "777-77-77"},
+            {id: 3, name: "Elena", number: "999-99-99"}
         ],
         filter: ""
     }
-    onInputFilter=(e)=>{
+    onInputFilter = (e) => {
         const value = e.currentTarget.value;
         this.setState({filter: value})
     }
-    onSubmit=(data)=>{
-        data.id =this.state.contacts.length+1;
-        this.setState({contacts: [...this.state.contacts , data]})
+    onSubmit = (data) => {
+        const tryName = this.state.contacts.map(c => c.name.toLowerCase())
+        data.id = this.state.contacts.length + 1;
+        if (tryName.includes(data.name.toLowerCase())) {
+            alert(`${data.name} Уже занято, введите другое имя!`)
+        } else {
+            this.setState({contacts: [...this.state.contacts, data]})
+        }
     }
-    onFilter=()=>{
-        const {filter,contacts}= this.state;
-        if(filter){
-            return contacts.filter(name =>name.name.toLowerCase().includes(filter));
-        }else {
+    onFilter = () => {
+        const {filter, contacts} = this.state;
+        if (filter) {
+            return contacts.filter(name => name.name.toLowerCase().includes(filter));
+        } else {
             return contacts;
         }
     }
+    onDelForId=(curentId)=>{
+        const list = this.state.contacts.filter(({id})=>id !== curentId);
+        this.setState({contacts: list});
+    }
+
     render() {
         const filteredContacts = this.onFilter();
         return (
@@ -46,6 +56,7 @@ export default class App extends Component {
                 />
                 <ContactList
                     contacts={filteredContacts}
+                    onDelForId={this.onDelForId}
                 />
             </div>
         )
--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,10 +1,13 @@
 import React from "react";
+import s from "./ContactList.module.css"
 
-export default function ContactList({contacts}) {
-    const render = contacts.map((contact)=><li key={contact.id}>{contact.name}: {contact.number}</li>)
+export default function ContactList({contacts, onDelForId}) {
+    const render = contacts.map((contact)=><li className={s.item} key={contact.id}><p>{contact.name}: {contact.number}</p><button className={s.btn} id={contact.id} type="button" onClick={()=>{onDelForId(contact.id)}}>Delete</button></li>)
         return(
             <div>
-                {render}
+                <ul>
+                    {render}
+                </ul>
             </div>
         )
 }
